d0f1018a00820f0e9c5c25f89f5a1975b38c8545
Tenerome <763263505@qq.com>
2021-09-19 21:55:55 +0800

    done

---
+++ java/测试this/test.java
@@ -0,0 +1,13 @@
+
+
+public class test {
+    public static void main(String args[]) {
+        thistest cc = new thistest();
+        int x = 89;
+        cc.setA(x);
+        System.out.println(cc.getA());
+        int y = 78;
+        cc.setB(y);
+        System.out.println(cc.getB());
+    }
+}
