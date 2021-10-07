076b4d7522f12143aa02d6e8166b0c4229b56753
Tenerome <763263505@qq.com>
2021-10-07 13:39:34 +0800

    doen

---
+++ webapp/demo/src/main/webapp/WEB-INF/classes/com/servlets/servlet.java
@@ -0,0 +1,22 @@
+package com.servlets;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+ 
+@WebServlet("/servlet")
+public class servlet extends HttpServlet {
+ 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        out.println("Hello,World");
+  
+    }
+ 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+ 
+}
