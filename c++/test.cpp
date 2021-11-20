47dafdcc4b88a36b19fea883bb3347926414b87c
Tenerome <763263505@qq.com>
2021-11-20 13:06:57 +0800

    done

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,38 +1,24 @@
-//对于父类函数（virtual、非virtual），如果有同型函数：
-//----非virtual函数由指针类型决定调用哪个
-//----virtual函数由指针指向的对象决定调用哪个（运行时决定）
 #include"iostream"
+#include"string"
+#include"string.h"
 using namespace std;
-class Base
-{
-public:
-    void func(int i){ cout <<"Base::func(int i)"<< endl; }
-     void func() {cout << "Base::func() " << endl;}
-    virtual void func2(int i) { cout << "Base::func2(int i)" << endl;}
-}; 
-
- 
-
-class Derived : public Base
-{  
-public: 
-      void func(){Base::func();}     
-      void func(int i){ cout <<"Derived::func()"<< endl; } 
-      void func2(int i){ cout <<"Derived::func2(int i)"<< endl; } 
-}; 
-
-int main()
-{
-    Base *pb = new Derived();
-    pb->func(1);  //Base::func(int i)
-    pb->func();  //Base:func()
-    pb->func2(1);  //Derived::func2(int i)
-    delete pb; 
-
-     Derived *pd = new Derived();
-    pd->func(1); //Derived::func(int i)
-     pd->func(); //不能调用 
-    pd->func2(1); //Derived::func2(int i)
-    delete pd;
-
+int count(char *substr,char *str){
+    int num,len;
+    len=strlen(substr);
+    char *p;
+    if(len<1)
+        return -1;
+    for(num=0,p=str;;){
+        p=strstr(p,substr);//strstr，在参数1中找到参数2并把其在1中的起始位置返回
+        if(p!=NULL){  //c++的空用大写NULL
+            num++;
+            p+=len;//找到之后，次数加一，p后移len个单位
+        }else
+            break;
+    }
+    return num;
+}
+int main(){
+    string a="ha",b="hahadwha";
+    cout<<count(&a[0],&b[0]);
 }
