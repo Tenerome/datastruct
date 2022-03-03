613b72be57540fb5fa23bd60e45c0f35788a99c5
Tenerome <763263505@qq.com>
2022-03-03 18:44:44 +0800

    done

--- datastructure/explore/树和二叉树/线索二叉树/ThreadTree.cpp
+++ datastructure/explore/树和二叉树/线索二叉树/ThreadTree.cpp
@@ -1,120 +1,92 @@
-//线索二叉树，中序
+//线索二叉树
 #include<iostream>
 using namespace std;
 
 class threadtree{//==存储结构==
     public:
-        char data;
-        threadtree * lp,* rp;
-        int ltag=0,rtag=0;//标志位默认为0
+    char data;
+    threadtree *lp,*rp;
+    int ltag,rtag;
 };
 
 typedef threadtree * Threadtree;
 
-void createTree(Threadtree &T){//==初始化==
-    T=(Threadtree)malloc(sizeof(threadtree));
-    T->data='A';
-    T->lp=(Threadtree)malloc(sizeof(threadtree));
-    T->rp=(Threadtree)malloc(sizeof(threadtree));
-
-    T->lp->data='B';
-    T->lp->lp=(Threadtree)malloc(sizeof(threadtree));
-    T->lp->rp=(Threadtree)malloc(sizeof(threadtree));
-
-    T->rp->data='C';
-    T->rp->lp=(Threadtree)malloc(sizeof(threadtree));
-    T->rp->rp=(Threadtree)malloc(sizeof(threadtree));
-
-    T->lp->lp->data='D';
-    T->lp->lp->lp=(Threadtree)malloc(sizeof(threadtree));
-    T->lp->lp->rp=(Threadtree)malloc(sizeof(threadtree));
-
-    T->lp->rp->data='E';
-    T->lp->rp->lp=(Threadtree)malloc(sizeof(threadtree));
-    T->lp->rp->rp=NULL;
-
-    T->rp->lp->data='F';
-    T->rp->lp->lp=NULL;
-    T->rp->lp->rp=NULL;
-
-    T->rp->rp->data='G';
-    T->rp->rp->lp=NULL;
-    T->rp->rp->rp=NULL;
-
-    T->lp->lp->lp->data='H';
-    T->lp->lp->lp->lp=NULL;
-    T->lp->lp->lp->rp=NULL;
-
-    T->lp->lp->rp->data='I';
-    T->lp->lp->rp->lp=NULL;
-    T->lp->lp->rp->rp=NULL;
-
-    T->lp->rp->lp->data='J';
-    T->lp->rp->lp->lp=NULL;
-    T->lp->rp->lp->rp=NULL;
-}
-
-void inThread(Threadtree &T,Threadtree &pre){//==建立中序线索二叉树==
-    if(T){//按照递归中序遍历的顺序建立线索
-        inThread(T->lp,pre);
-        if(!T->lp){//当前节点左孩子为空
-            T->lp=pre;
-            T->ltag=1;
-        }
-        if(!pre->rp){//前驱节点右孩子为空
-            pre->rp=T;//(前驱节点右孩子相当于不用返回到前驱操作，在操作左孩子的同时也操作右孩子)
-            pre->rtag=1;
-        }
-        pre=T;
-        inThread(T->rp,pre);
+void CreateTree(Threadtree &T){//==先序序列建立二叉树==
+    char ch;
+    cin>>ch;//cin也可以直接输入字符串自动读取单个字符
+    if(ch=='#')
+        T=nullptr;
+    else{
+        T=(Threadtree)malloc(sizeof(threadtree));
+        T->data=ch;
+        T->ltag=0;
+        T->rtag=0;
+        CreateTree(T->lp);
+        CreateTree(T->rp);
     }
 }
 
-void createThread(Threadtree &T){//==建立线索二叉树==
-    Threadtree pre=NULL;//头节点的前驱为空
-    if(T){
-        inThread(T,pre);
-        pre->rp=NULL;//最后一个节点，单独处理，尾节点的后继为空
+void inThread(Threadtree &T,Threadtree &pre){//==线索化==
+    if(!T)
+        return;
+    inThread(T->lp,pre);
+
+    if(!T->lp){
+        T->ltag=1;
+        T->lp=pre;
+    }
+    if(!pre->rp){
         pre->rtag=1;
+        pre->rp=T;
+    }
+
+    pre=T;
+    inThread(T->rp,pre);
+}
+
+void InOrderThread(Threadtree &p,Threadtree &T){//p是头节点，并和T连接起来
+    p=(Threadtree)malloc(sizeof(threadtree));//新建头节点
+    p->ltag=0;
+    p->rtag=1;
+    p->rp=p;
+    if(!T){
+        p->lp=p;
+        return; //根节点不存在，则让头节点左孩子指向自身
+    }
+    Threadtree pre;
+    pre=p;
+    p->lp=T;//头节点左孩子指向根节点
+    inThread(T,pre);
+    pre->rp=p;
+    pre->rtag=1;
+    p->rp=pre;
+}
+
+void InOrderTracerse(Threadtree &T){//非递归遍历线索二叉树
+    Threadtree p=T->lp;
+    while(p!=T){
+        while(p->ltag==0)
+            p=p->lp;
+        while(p->rtag==1&&p->rp!=T){
+            p=p->rp;
+            cout<<p->data;
+        }
+        p=p->rp;
     }
 }
 
-Threadtree  firstNode(Threadtree &T){//==求中序序列的头节点==
-    while(!T->ltag){
-        T=T->lp;
-    }
-    return T;
-}
-
-Threadtree nextNode(Threadtree &T){//==求后继节点==
-    if(!T->rtag){
-        return firstNode(T->rp);//若无线索,则找右子树的最左下节点，就是右孩子非空，则其后继是其右子树的最左下的节点
-    }else{
-        return T->rp;
-    }
-}
-
-void inOrder(Threadtree &T){//==遍历线索二叉树==
-    Threadtree p=firstNode(T);
-    for(;p!=NULL;p=nextNode(p)){
-        cout<<p->data<<" ";
-    }
-}
-
-void inOrder2(Threadtree &T){//==递归遍历二叉树，用作对比==
-    cout<<"递归遍历二叉树:";
-    if(T!=NULL){
-        inOrder2(T->lp);
+void inOrder(Threadtree &T){//test--ok，递归遍历二叉树
+    if(T){
         cout<<T->data<<" ";
-        inOrder2(T->rp);
+        inOrder(T->lp);
+        inOrder(T->rp);
     }
-    cout<<endl;
 }
-int main(){
-    Threadtree T;
-    createTree(T);
-    inOrder2(T);
-    createThread(T);
-    inOrder(T);
 
+int main(){
+    Threadtree T,p;
+    CreateTree(T);
+    inOrder(T);
+    InOrderThread(p,T);
+    InOrderTracerse(p);
 }
