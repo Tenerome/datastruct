21f7b70c42aa4499cc08bb12459144eb51ca85f6
Tenerome <763263505@qq.com>
2022-03-06 16:50:33 +0800

    done

--- c++/test.cpp
+++ c++/test.cpp
@@ -4,10 +4,7 @@
 using namespace std;
 
 int main(){
+    float cc=0.092;
     int a;
-    cout<<"删除第";
-    cout<<" "<<"位访问者";
-    printf("\b\b\b\b\b\b\b\b\b");
-    cin>>a;
-    cout<<"第"<<a<<"位已删除";
+    cout<<a;
 }
