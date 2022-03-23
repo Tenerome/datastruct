465f4a3f4f52c973bfd2e829f4f3735023ab9f61
Tenerome <763263505@qq.com>
2022-03-23 20:14:17 +0800

    done

---
+++ java/基础学java/test.java
@@ -0,0 +1,14 @@
+
+public class test{
+    int x;
+    public test(){
+        this.x=0;
+    }
+    public test(int x){
+        this.x=x;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
