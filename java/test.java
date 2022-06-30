52f6d0feff28c2b7c522f21c8f4010414d7a039c
Tenerome <763263505@qq.com>
2022-06-30 19:16:54 +0800

    done

---
+++ java/test.java
@@ -0,0 +1,11 @@
+import javax.sound.midi.SysexMessage;
+
+import com.mysql.cj.protocol.x.SyncFlushDeflaterOutputStream;
+
+class test{
+    public static void main(String[] args) {
+        int x;
+        x=()->5;
+        System.out.println(x);
+    }
+}
