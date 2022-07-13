2bfc0b49f9ced7c64a037de47715c2c6d1d33dc7
ChenHeng <Linux Mint OS>
2022-07-13 15:54:29 +0800

    done

--- java/基础学java/test.java
+++ java/基础学java/test.java
@@ -1,14 +1,14 @@
 
-public class test{
-    int x;
-    public test(){
-        this.x=0;
+public class test{//一个class类
+    public static int add(int x,int y){//add()函数
+        return x+y;
     }
-    public test(int x){
-        this.x=x;
+    public static void print(int x){//print()函数
+        System.out.println(x);//java常用的输出语句
     }
-
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {//主函数
+        int a=12,b=15;
+        int c=add(a,b);//调用add
+        print(c);//调用print
     }
 }
